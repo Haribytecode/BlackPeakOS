ada63692ee8bea8c9c8c881eda37304a6d52afe9
Haribytecode <osdevhari@gmail.com>
2026-09-13 22:13:31 +0530

    panic: add panic_at with file/line for VFS debugging

--- panic.c
+++ panic.c
@@ -1,5 +1,6 @@
 #include "panic.h"
 #include "console.h"
+#include "uart.h"
 extern void uart_puts(const char *);
 void panic(const char *msg){
     uart_puts("KERNEL PANIC\n");
@@ -7,5 +8,18 @@ void panic(const char *msg){
     uart_puts("SYSTEM HALTED\n");
     while(1){
         __asm__ volatile("cli;hlt");
+    }
+}
+void panic_at(const char *msg, const char *file, int line)
+{
+    uart_puts("KERNEL PANIC: ");
+    uart_puts(msg);
+    uart_puts("\n  at ");
+    uart_puts(file);
+    uart_puts(":");
+    uart_puthex((uint32_t)line);
+    uart_puts("\nSYSTEM HALTED\n");
+    while (1) {
+        __asm__ volatile("cli; hlt");
     }
 }
--- panic.h
+++ panic.h
@@ -1,2 +1,4 @@
 #pragma once
 void panic(const char *msg);
+void panic_at(const char *msg, const char *file, int line);
+#define PANIC(msg) panic_at((msg), __FILE__, __LINE__)
